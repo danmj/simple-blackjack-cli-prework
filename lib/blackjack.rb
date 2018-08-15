138a82bef2e38b39aa7d21d7674dc769f85b0c3a
danmj <daniel.jung@stern.nyu.edu>
2018-08-15 17:58:51 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,7 +39,7 @@ def hit?(card_total)
   prompt_user
   input = get_user_input
   if input == 'h'
-    card total += deal_card
+    card total = card_total + deal_card
     elsif input == 's'
     0 + card_total
   end
